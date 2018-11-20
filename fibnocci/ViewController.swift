62c9ffd5e3272b10f87ecc471fa8ff26a1459728
Siva Kumar <sivakumar@sivakumars-macbook-air.local>
2018-11-20 22:05:37 +0530

    fibonacci series

--- fibnocci/ViewController.swift
+++ fibnocci/ViewController.swift
@@ -13,8 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        fibnocci(n: 10)
     }
 
 
+    func fibnocci(n:Int){
+        
+        var n1 = 0 , n2 = 1
+        var n3:Int
+        
+        for _ in 1...n{
+            
+            print(n1)
+            n3 = n1 + n2
+            n1 = n2
+            n2 = n3
+            
+            
+            
+        }
+        
+        
+    }
 }
 
